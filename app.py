5dc9b1ad2c6f4a6d8f0f3987728ef2e5ffc9c515
SummerGuan <125408708+SummerGuan@users.noreply.github.com>
2023-02-14 13:52:40 -0800

    update

--- app.py
+++ app.py
@@ -6,8 +6,18 @@ st.write("APP WORKING!")
 tickers = ['AAPL','DIS','IBM']
 ticker = st.selectbox("Pick a ticker", tickers)
 
-df = pd.read_csv(ticker + ".csv", parse_dates=['Date'], index_col =['Date'] )
+df = pd.read_csv(ticker + ".csv",
+parse_dates=['Date'],
+index_col =['Date'] )
+
+begDate = df.index.min()
+endDate = df.index.max()
 
 st.write(df)
+st.write(begDate)
+st.write(endDate)
+
+
+
 
 
